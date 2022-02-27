02bc50be3025337534b6241a21b7539d0609e0df
mikeolivier <97438090+mikeolivier@users.noreply.github.com>
2022-02-27 14:24:16 -0500

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,10 +1,4 @@
 #!/bin/bash
-
-        num1 = 1.5
-        num2 = 6.3
-
-        # Add two numbers
-        sum = num1 + num2
-
-        # Display the sum
-        print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+echo "Printing text with newline"
+echo -n "Printing text without newline"
+echo -e "\nRemoving \t backslash \t characters\n"
