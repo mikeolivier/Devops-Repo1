a826a36f13cb3d5513bc9f49e0d38eb2ff9deee8
mikeolivier <mytalent2022@gmail.com>
2022-03-13 03:57:36 +0000

    tomcat_user_config

---
+++ file1.sh
@@ -0,0 +1,10 @@
+
+## Tomcat user configuration
+
+<!--
+<role rolename="manager-gui"/>
+<role rolename="manager-script"/>
+<role rolename="manager-jmx"/>
+<role rolename="manager-status"/>
+<user username="admin"  password="12345" roles="manager-gui,manager-script,manager-jmx,manager-status"/>
+<user username="tomcat" password="tomcat12345" roles="manager-gui"/>
